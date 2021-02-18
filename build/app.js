39d1f08ea59eab84190469c9d88dd4224315ff5f
Xavier Burrow <xavierb@temphost.local>
2021-02-18 14:38:43 +1100

    ?

--- build/app.js
+++ build/app.js
@@ -2,6 +2,17 @@
 
 class App {
   constructor() {
+    this.el = {};
+    this.el.tab = document.querySelector('#tab');
+    this.el.inputTranspose = document.querySelector('input[name="transpose"]');
+
+    // bind
+    this.transpose = 0;
+    this.el.inputTranspose.addEventListener('change', () => {
+      this.transpose = parseInt(this.el.inputTranspose.value);
+    });
+
+    // get example tab
     fetch('../example/synapse.txt')
       .then(res => res.text())
       .then(text => {
@@ -10,7 +21,27 @@ class App {
   }
 
   parse(text) {
-    let rows = text.split('\n');
+    this.tab = [];
+    
+    this.rows = text.split('\n');
+    this.rows.forEach((row, index) => {
+      let i = 0;
+      while (i < row.length) {
+        // check if number
+        if (isNaN(row[i])) {
+          i += 1;
+          continue;
+        }
+        // check is in tab
+        let isTab = (i > 0 && row[i-1] )
+      }
+    });
+
+    this.tab = text.split('\n').map(row => {
+      return row.split('').map(char => {
+
+      });
+    });
 
     // 1. split rows
     // 2. find numbers (up to 2-digits)
@@ -18,7 +49,11 @@ class App {
     // get props i.e. semis till next string
     // 4. reform text
 
-    document.querySelector('#tab').innerText = chars.join('');
+    this.render();
+  }
+
+  render() {
+    let text =
   }
 }
 
